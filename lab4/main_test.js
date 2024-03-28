a89ae666550d5a9a1459ff9f8657e556347deb46
googlucky007 <58160751+googlucky007@users.noreply.github.com>
2024-03-28 16:26:18 +0800

    Update main_test.js

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -8,14 +8,28 @@ const puppeteer = require('puppeteer');
     // Navigate the page to a URL
     await page.goto('https://pptr.dev/');
 
-    // Hints:
     // Click search button
+    await page.click('.searchbox [type="submit"]');
+
     // Type into search box
+    await page.type('.searchbox input', 'search query');
+
     // Wait for search result
+    await page.waitForSelector('.search-results');
+
     // Get the `Docs` result section
+    const docsSection = await page.$('.search-results .section[data-section="Docs"]');
+
     // Click on first result in `Docs` section
+    const firstResult = await docsSection.$('.search-results-item');
+    await firstResult.click();
+
     // Locate the title
+    const titleElement = await page.$('h1');
+    const title = await page.evaluate(titleElement => titleElement.textContent, titleElement);
+
     // Print the title
+    console.log('Title:', title);
 
     // Close the browser
     await browser.close();
