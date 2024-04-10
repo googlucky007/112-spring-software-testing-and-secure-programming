4f364f2368712cf0e19065bed7aa76e5e3ee2362
googlucky007 <58160751+googlucky007@users.noreply.github.com>
2024-04-10 19:08:20 +0800

    Update main_test.js

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -1,36 +1,43 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    // Launch the browser and open a new blank page
+    // 啟動瀏覽器並開啟新的空白頁面
+    // 啟動瀏覽器並開啟新的空白頁面 const browser = await puppeteer.launch({headless:false, executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'});
     const browser = await puppeteer.launch();
+  
     const page = await browser.newPage();
 
-    // Navigate the page to a URL
+    // 將頁面導覽到某個 URL
     await page.goto('https://pptr.dev/');
 
-    // Click search button
-    await page.click('.searchbox [type="submit"]');
+    // 設定螢幕尺寸
+    await page.setViewport({width: 1080, height: 1024});
 
-    // Type into search box
-    await page.type('.searchbox input', 'search query');
+    // 等待並點擊第一個結果
+    const button = await page.waitForSelector('.DocSearch-Button', {visible: true});
+    await button.click();
 
-    // Wait for search result
-    await page.waitForSelector('.search-results');
+    // 在搜尋框中輸入
+    const input = await page.waitForSelector('#docsearch-input', {visible: true});
+    await input.type('chipi chipi chapa chapa');
+    await new Promise(resolve => setTimeout(resolve, 1000));
 
-    // Get the `Docs` result section
-    const docsSection = await page.$('.search-results .section[data-section="Docs"]');
+    //透過id來識別元素
+    const selector = await page.waitForSelector('#docsearch-item-5');
+    const handle = await selector.$$('a');
+    const propertyHandles = await Promise.all(handle.map(h => h.getProperty('href')));
+    const targetHref = await Promise.all(propertyHandles.map(h => h.jsonValue()));
+	
+    //導航到targetHref數組中的第一個 URL 並等待頁面完全加載，再繼續執行進一步操作
+    await page.goto(targetHref[0]);
 
-    // Click on first result in `Docs` section
-    const firstResult = await docsSection.$('.search-results-item');
-    await firstResult.click();
+    // 使用 CSS 選擇器擷取單一元素
+	// 選擇器範例：h7
+    const element = await page.$('h7'); 
 
-    // Locate the title
-    const titleElement = await page.$('h1');
-    const title = await page.evaluate(titleElement => titleElement.textContent, titleElement);
+    // 擷取並記錄元素的文字內容
+    const elementText = await page.evaluate(element => element.textContent, element);
+    console.log(elementText);
 
-    // Print the title
-    console.log('Title:', title);
-
-    // Close the browser
     await browser.close();
 })();
